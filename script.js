09e7034858efe66c86c303af05dd941091374b14
drianlarde <aadabelarde2020@plm.edu.ph>
2023-03-17 02:53:21 +0800

    Major Changes

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-var image = document.getElementsByClassName('me-image');
+var image = document.getElementsByClassName("me-image");
 
 new simpleParallax(image, {
-    delay: .6,
-    transition: 'cubic-bezier(0,0,0,1)',
-    // Make it strong
-    scale: 2
+  delay: 0.6,
+  transition: "cubic-bezier(0,0,0,1)",
+  scale: 2,
 });
 
+// This gets the img child elements of .gallery-grid-wrapper class
+var galleryGridWrapper = document.getElementsByClassName("gallery-grid-wrapper");
+var galleryGridWrapperImg = galleryGridWrapper[0].getElementsByTagName("img");
+
+// This loops through the images and add the simpleParallax
+for (var i = 0; i < galleryGridWrapperImg.length; i++) {
+  new simpleParallax(galleryGridWrapperImg[i], {
+    delay: 1,
+    transition: "cubic-bezier(0,0,0,1)",
+    scale: 1.2,
+  });
+}
